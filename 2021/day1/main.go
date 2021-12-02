649cf647ff51238cda5720f02be16f6b7c693d44
Tenebriso <emilia.voicu95@gmail.com>
2021-12-02 13:37:06 +0200

    refactor

--- 2021/day1/main.go
+++ 2021/day1/main.go
@@ -8,32 +8,29 @@ import (
 	"strconv"
 )
 
-func first(fn string) int {
-	file, err := os.Open(fn)
-	if err != nil {
-		log.Fatal(err)
+func first(prevDepth *int, depth int, totalIncreases *int) {
+	if *prevDepth != 0 && *prevDepth < depth {
+			*totalIncreases++
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	var prevDepth, totalIncreases = 0, 0
-	
-	for scanner.Scan() {
-		depth, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			log.Fatal(err)
-		}
-		if prevDepth != 0 && prevDepth < depth {
-			totalIncreases++
-		}
-		prevDepth = depth
-	}
-
-	return totalIncreases
+	*prevDepth = depth
 }
 
-func second(fn string) int {
+func second(idx *int, currentDepth *int, prevDepth *int, depth int, window *[]int, totalIncreases *int) {
+	(*idx)++
+	(*currentDepth) += depth
+	if *idx <= 3 {    // first sum
+		*window = append(*window, depth)
+	} else {
+		*currentDepth -= (*window)[*idx%3]
+		if *currentDepth > *prevDepth {
+			*totalIncreases++
+		}
+		(*window)[*idx%3] = depth
+	}
+	*prevDepth = *currentDepth
+}
+
+func solve(fn string, part int) int {
 	file, err := os.Open(fn)
 	if err != nil {
 		log.Fatal(err)
@@ -44,34 +41,26 @@ func second(fn string) int {
 
 	var prevDepth, currentDepth, totalIncreases, idx = 0, 0, 0, 0
 	var window []int
-	
+
 	for scanner.Scan() {
 		depth, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		idx++
-		currentDepth += depth
-		if idx <= 3 {
-			window = append(window, depth)
-		} else {
-			currentDepth -= window[idx%3]
-			if currentDepth > prevDepth {
-				totalIncreases++
-			}
-			window[idx%3] = depth
+		if part == 1 {
+			first(&prevDepth, depth, &totalIncreases)
+		} else if part == 2{
+			second(&idx, &currentDepth, &prevDepth, depth, &window, &totalIncreases)
 		}
-		prevDepth = currentDepth
 	}
 
 	return totalIncreases
 }
 
 func main() {
-	// day 1
 	fn := "input.txt"
-	fmt.Println(first(fn))
-
+	// day 1
+	fmt.Println(solve(fn, 1))
 	// day 2
-	fmt.Println(second(fn))
+	fmt.Println(solve(fn, 2))
 }
