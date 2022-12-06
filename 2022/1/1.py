faaa52a16e3ae687bc6d5c943bd0827aee3e1278
Emilia Voicu <emilia.voicu@bolt.eu>
2022-12-06 10:45:09 +0200

    combine functions

--- 2022/1/1.py
+++ 2022/1/1.py
@@ -1,22 +1,7 @@
 import heapq
 
 
-def find_max():
-    max_cals = 0
-    with open('input') as fp:
-        current_cals = 0
-        for line in fp:
-            line = line.strip()
-            if not line:
-                if max_cals < current_cals:
-                    max_cals = current_cals
-                current_cals = 0
-            else:
-                current_cals += int(line)
-    return max_cals
-
-
-def find_max_three():
+def find_max_n(n=1):
     all_calories = []
     with open('input') as fp:
         current_cals = 0
@@ -27,9 +12,9 @@ def find_max_three():
                 current_cals = 0
             else:
                 current_cals += int(line)
-    return heapq.nlargest(3, all_calories)
+    return heapq.nlargest(n, all_calories)
 
 
 if __name__ == '__main__':
-    print("Part 1: " + str(find_max()))
-    print("Part 2: " + str(sum(find_max_three())))
+    print("Part 1: " + str(sum(find_max_n())))
+    print("Part 2: " + str(sum(find_max_n(3))))
